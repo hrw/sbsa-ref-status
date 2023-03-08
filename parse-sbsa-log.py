5bd744f0a96217a76e4029ea212c521c8de580f4
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2023-03-08 19:54:02 +0100

    add script to parse SBSA ACS logs
    For now only tested on level 7 logs.
    
    TODO: add SBSA level information

---
+++ parse-sbsa-log.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+import sys
+import yaml
+
+with open("status-sbsa.yml", encoding="utf-8") as yml:
+    status_sbsa = yaml.safe_load(yml)
+
+if len(sys.argv) < 3:
+    print("Give me: logfile corename")
+    sys.exit(-1)
+
+core_name = sys.argv[2]
+
+start_parsing = False
+
+with open(sys.argv[1], encoding="utf-8") as sbsa_acs_log:
+
+    test_id = 0
+    test_title = ""
+    prev_line = False
+    while True:
+        line = sbsa_acs_log.readline()
+
+        if not start_parsing and "Starting PE tests" in line:
+            start_parsing = True
+
+        if start_parsing:
+            if "START" in line:
+                test_id, test_title = prev_line.removesuffix("START\n").strip().split(" : ")
+
+            if test_id and line.strip().startswith(("B_", "IE_", "ITS_",
+                                                    "PCI_", "PMU_", "RE_",
+                                                    "S_")):
+                test_tags = line.strip()
+                print(f"{test_id}\t{test_tags}\t{test_title}")
+                try:
+                    status_sbsa[int(test_id)]["tags"] = test_tags
+                    status_sbsa[int(test_id)]["title"] = test_title
+                except KeyError:
+                    status_sbsa[int(test_id)] = {"tags": "", "title": "",
+                                                 "status": {core_name: ""}
+                                                 }
+                    status_sbsa[int(test_id)]["tags"] = test_tags
+                    status_sbsa[int(test_id)]["title"] = test_title
+
+
+            if 'Result:' in line:
+                result = line.split(':')[-1].strip()
+                try:
+                    status_sbsa[int(test_id)]["status"][core_name] = result
+                except KeyError:
+                    status_sbsa[int(test_id)] = {"tags": "", "title": "",
+                                                 "status": {core_name: ""}
+                                                 }
+                    status_sbsa[int(test_id)]["status"][core_name] = result
+
+
+            if line.strip().startswith("Total Tests run"):
+                break
+
+        prev_line = line
+
+with open("status-sbsa.yml", "w") as yml:
+    yaml.dump(status_sbsa, yml)
