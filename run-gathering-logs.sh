36db9717dfe33e6b441eedcb115cc88b82b214ae
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2023-03-09 19:22:54 +0100

    gather BSA ACS run logs
    Without SBSA mode (so no "-sbsa" switch).
    
    Closes: #1

--- run-gathering-logs.sh
+++ run-gathering-logs.sh
@@ -2,8 +2,11 @@
 
 for cpu in cortex-a57 cortex-a72 neoverse-n1 max
 do
-	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi -sbsa" 2>&1 |grep -v " Node :" | tee logs/bsa-${cpu}.log
-	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi -sbsa -v 1" 2>&1 |grep -v " Node :" | tee logs/bsa-${cpu}-v1.log
+	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi" 2>&1 |grep -v " Node :" | tee logs/bsa-${cpu}.log
+	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi -v 1" 2>&1 |grep -v " Node :" | tee logs/bsa-${cpu}-v1.log
+
+	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi -sbsa" 2>&1 |grep -v " Node :" | tee logs/bsa-sbsa-${cpu}.log
+	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi -sbsa -v 1" 2>&1 |grep -v " Node :" | tee logs/bsa-sbsa-${cpu}-v1.log
 
 	for level in 3 4 5 6 7
 	do
--- run-parsing-logs.sh
+++ run-parsing-logs.sh
@@ -3,6 +3,7 @@
 for cpu in cortex-a57 cortex-a72 neoverse-n1 max
 do
 	python3 parse-bsa-log.py  logs/bsa-${cpu}-v1.log $cpu
+	python3 parse-bsa-log.py  logs/bsa-sbsa-${cpu}-v1.log $cpu
 
 	for level in 3 4 5 6 7
 	do
