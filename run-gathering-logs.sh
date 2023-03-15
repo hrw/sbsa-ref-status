7d519de49c00734ed2e8ba5f753d84ad98f14512
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2023-03-15 11:49:10 +0100

    collect logs quietly

--- run-gathering-logs.sh
+++ run-gathering-logs.sh
@@ -2,17 +2,27 @@
 
 for cpu in cortex-a57 cortex-a72 neoverse-n1 max
 do
-	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi" 2>&1 |grep -v " Node :" | tee logs/bsa-${cpu}.log
-	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi -v 1" 2>&1 |grep -v " Node :" | tee logs/bsa-${cpu}-v1.log
+	printf "Run tests for %11b: " $cpu
 
-	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi -sbsa" 2>&1 |grep -v " Node :" | tee logs/bsa-sbsa-${cpu}.log
-	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi -sbsa -v 1" 2>&1 |grep -v " Node :" | tee logs/bsa-sbsa-${cpu}-v1.log
+	echo -n "BSA normal"
+	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi" 2>&1 |grep -v " Node :" >> logs/bsa-${cpu}.log
+	echo -n "/v "
+	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi -v 1" 2>&1 |grep -v " Node :" >> logs/bsa-${cpu}-v1.log
 
+	echo -n "sbsa"
+	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi -sbsa" 2>&1 |grep -v " Node :" >> logs/bsa-sbsa-${cpu}.log
+	echo -n "/v "
+	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi -sbsa -v 1" 2>&1 |grep -v " Node :" >> logs/bsa-sbsa-${cpu}-v1.log
+
+	echo -n "SBSA level "
 	for level in 3 4 5 6 7
 	do
-		./boot-sbsa-ref.sh $cpu nogfx "fs0:sbsa.efi -l ${level}" 2>&1 |grep -v " Node :" | tee logs/sbsa-${cpu}-${level}.log
-		./boot-sbsa-ref.sh $cpu nogfx "fs0:sbsa.efi -l ${level} -v 1" 2>&1 |grep -v " Node :" | tee logs/sbsa-${cpu}-${level}-v1.log
+		echo -n $level
+		./boot-sbsa-ref.sh $cpu nogfx "fs0:sbsa.efi -l ${level}" 2>&1 |grep -v " Node :" >> logs/sbsa-${cpu}-${level}.log
+		echo -n "/v "
+		./boot-sbsa-ref.sh $cpu nogfx "fs0:sbsa.efi -l ${level} -v 1" 2>&1 |grep -v " Node :" >> logs/sbsa-${cpu}-${level}-v1.log
 	done
 
-	./boot-sbsa-ref.sh $cpu nogfx "acpiview" 2>&1 |grep -v " Node :" | tee logs/acpiview-${cpu}.log
+	echo "ACPI tables"
+	./boot-sbsa-ref.sh $cpu nogfx "acpiview" 2>&1 |grep -v " Node :" >> logs/acpiview-${cpu}.log
 done
