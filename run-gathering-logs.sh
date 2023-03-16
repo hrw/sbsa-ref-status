9688c36c861146808b7928fa76e31e5fdcf702e5
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2023-03-16 10:54:21 +0100

    gathering logs was appending instead of writing

--- run-gathering-logs.sh
+++ run-gathering-logs.sh
@@ -5,24 +5,24 @@ do
 	printf "Run tests for %11b: " $cpu
 
 	echo -n "BSA normal"
-	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi" 2>&1 |grep -v " Node :" >> logs/bsa-${cpu}.log
+	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi" 2>&1 |grep -v " Node :" > logs/bsa-${cpu}.log
 	echo -n "/v "
-	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi -v 1" 2>&1 |grep -v " Node :" >> logs/bsa-${cpu}-v1.log
+	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi -v 1" 2>&1 |grep -v " Node :" > logs/bsa-${cpu}-v1.log
 
 	echo -n "sbsa"
-	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi -sbsa" 2>&1 |grep -v " Node :" >> logs/bsa-sbsa-${cpu}.log
+	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi -sbsa" 2>&1 |grep -v " Node :" > logs/bsa-sbsa-${cpu}.log
 	echo -n "/v "
-	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi -sbsa -v 1" 2>&1 |grep -v " Node :" >> logs/bsa-sbsa-${cpu}-v1.log
+	./boot-sbsa-ref.sh $cpu nogfx "fs0:bsa.efi -sbsa -v 1" 2>&1 |grep -v " Node :" > logs/bsa-sbsa-${cpu}-v1.log
 
 	echo -n "SBSA level "
 	for level in 3 4 5 6 7
 	do
 		echo -n $level
-		./boot-sbsa-ref.sh $cpu nogfx "fs0:sbsa.efi -l ${level}" 2>&1 |grep -v " Node :" >> logs/sbsa-${cpu}-${level}.log
+		./boot-sbsa-ref.sh $cpu nogfx "fs0:sbsa.efi -l ${level}" 2>&1 |grep -v " Node :" > logs/sbsa-${cpu}-${level}.log
 		echo -n "/v "
-		./boot-sbsa-ref.sh $cpu nogfx "fs0:sbsa.efi -l ${level} -v 1" 2>&1 |grep -v " Node :" >> logs/sbsa-${cpu}-${level}-v1.log
+		./boot-sbsa-ref.sh $cpu nogfx "fs0:sbsa.efi -l ${level} -v 1" 2>&1 |grep -v " Node :" > logs/sbsa-${cpu}-${level}-v1.log
 	done
 
 	echo "ACPI tables"
-	./boot-sbsa-ref.sh $cpu nogfx "acpiview" 2>&1 |grep -v " Node :" >> logs/acpiview-${cpu}.log
+	./boot-sbsa-ref.sh $cpu nogfx "acpiview" 2>&1 |grep -v " Node :" > logs/acpiview-${cpu}.log
 done
