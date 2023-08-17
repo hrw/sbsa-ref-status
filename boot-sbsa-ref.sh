273b8b22c5ac49ad3adadb25e5202409cefed8fd
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2023-08-17 13:49:44 +0200

    boot-sbsa-ref.sh: rework to have saner default and arguments
    Instead of relying on positional arguments script now takes set of
    arguments:
    
    --cmd    - command to run in UEFI shell
    --cpu    - cpu core to use (neoverse-v1 is default)
    --no-its - do not add complex PCIe setup
    --reset  - quit emulator after running command
    --gfx    - open UI window for graphics output (off by default)
    --gdb    - wait for GDB to connect
    
    Shell command will be run in 100x31 mode and PCI setup will be printed
    first.
    
    QEMU command line is printed before run.

--- boot-sbsa-ref.sh
+++ boot-sbsa-ref.sh
@@ -1,56 +1,87 @@
 #!/bin/bash
 
-cpu="max"
-extra_opts=""
+CPU="neoverse-v1"
 
-if [ ! -z $1 ]; then
-        cpu=$1
-fi
+for i in "$@"; do
+	case $i in
+		--cmd=*)
+			CMD="${i#*=}"
+			shift
+			;;
+		--cpu=*)
+			CPU="${i#*=}"
+			shift
+			;;
+		--no-its)
+			NOITS=1
+			shift
+			;;
+		--reset)
+			RESET=1
+			shift
+			;;
+		--gfx)
+			GFX=1
+			shift
+			;;
+		--gdb)
+			GDB=1
+			shift
+			;;
+		-*|--*)
+			echo "Unknown option $i"
+			exit 1
+			;;
+		*)
+			;;
+	esac
+done
 
-if [ ! -z $2 ]; then
-        extra_opts="-nographic"
-fi
+rm -f disks/virtual/startup.nsh
 
-if [ ! -z "$3" ]; then
-        echo "mode 100 31" > disks/virtual/startup.nsh
-        echo "pci"        >> disks/virtual/startup.nsh
-        echo "$3"         >> disks/virtual/startup.nsh
-    if [ -z "$4" ]; then
-        echo "reset -c"   >> disks/virtual/startup.nsh
+if [ ! -z "$CMD" ]; then
+        echo "mode 100 31"  > disks/virtual/startup.nsh
+        echo "pci"         >> disks/virtual/startup.nsh
+        echo "$CMD"        >> disks/virtual/startup.nsh
+    if [ ! -z "$RESET" ]; then
+        echo "reset -c"    >> disks/virtual/startup.nsh
     fi
 fi
 
-qemu_args=(
+common_qemu_args=(
 
 -machine sbsa-ref
 -m 4096
--smp 4
--cpu $cpu
+-smp 2
+-cpu $CPU
 
 # firmware
 -drive if=pflash,file=SBSA_FLASH0.fd,format=raw
 -drive if=pflash,file=SBSA_FLASH1.fd,format=raw
 
 # basic disk with EFI apps and generic Debian d-i
-#-drive file=fat:rw:$PWD/disks/virtual/,format=raw
+-drive file=fat:rw:$PWD/disks/virtual/,format=raw
 
 # full Debian installation
--drive format=qcow2,file=$PWD/../disks/full-debian.qcow2
+-drive format=raw,file=$PWD/disks/full-debian.hddimg
 
 # Debian install ISO
 #-drive if=ide,file=$PWD/../disks/debian-bookworm.iso,format=raw,media=cdrom
 
+# Fedora install ISO
+#-drive if=ide,file=$PWD/../disks/Fedora-Server-netinst-aarch64-Rawhide-20230704.n.0.iso,format=raw,media=cdrom
+
+# ServerReady ACS
+#  -drive file=../disks/sr_acs_live_image-v23.01_2.0.0_BETA-0.img,format=raw,if=ide
+
 # userspace networking
--net nic
--net user
+#-net nic
+#-net user
 
 # usb for graphical console
--usb
+#-usb
 -device usb-kbd
 -device usb-tablet
-
-# any extra options set by script above
-$extra_opts
 
 -watchdog-action none
 
@@ -63,30 +94,8 @@ $extra_opts
 # output on console
 -serial stdio
 
-# --------------------------------------------------------------------------------------
-# now some PCIe stuff I normally do not use
-# --------------------------------------------------------------------------------------
-
-# some PCIe switch with ac97 behind it
-# -device pcie-root-port,id=root_port_for_switch1,chassis=1,slot=12
-#   -device x3130-upstream,id=upstream_port1,bus=root_port_for_switch1
-#     -device xio3130-downstream,id=downstream_port1,bus=upstream_port1,chassis=1,slot=20
-#       -device ac97,bus=downstream_port1
-
-# NVME card
-# -device pcie-root-port,id=root_port_for_nvme1,chassis=1,slot=1
-#   -device nvme,serial=deadbeef,drive=nvme,bus=root_port_for_nvme1
-
-# PCIe-PCI bridge with virtio-rng behind
-# -device pcie-root-port,id=root_port_for_pci,chassis=1,slot=2
-#   -device pcie-pci-bridge,id=pci,bus=root_port_for_pci
-#     -device virtio-rng-pci,bus=pci,addr=8
-
-# another PCIe bus
-# -device pxb-pcie,id=pxb1,bus_nr=2
-#   -device pcie-root-port,id=root_port_for_ahci,bus=pxb1,chassis=2,slot=1
-#     -device ahci,bus=root_port_for_ahci
-
+# Intel network card to have interrupt debugging information
+-device igb
 
 # --------------------------------------------------------------------------------------
 # some random disks/isos
@@ -99,10 +108,79 @@ $extra_opts
 # -drive file=disks/alpine-standard-3.17.2-aarch64.iso,format=raw,media=cdrom
 
 # -drive if=ide,format=raw,file=disks/luv-live-image-gpt.img
+
 )
 
-./code/qemu/build/aarch64-softmmu/qemu-system-aarch64 ${qemu_args[@]}
+its_pci_setup=(
 
-if [ ! -z "$3" ]; then
+# --------------------------------------------------------------------------------------
+# now some PCIe stuff for system with ITS
+# --------------------------------------------------------------------------------------
+
+# 104c:8232 x3130-upstream  
+# 1274:5000 es1370          
+# 1af4:1044 virtio-rng-pci  
+# 1b36:000c pcie-root-port  
+# 1b36:000e pcie-pci-bridge 
+# 1b36:0010 qemu nvme       
+# 8086:10c9 intel igb       
+# 8086:2922 ahci            
+
+# PCIe switch with ac97 behind it
+-device pcie-root-port,id=root_port_for_switch1,chassis=0,slot=0
+  -device x3130-upstream,id=upstream_port1,bus=root_port_for_switch1
+    -device xio3130-downstream,id=downstream_port1,bus=upstream_port1,chassis=1,slot=0
+      -device ac97,bus=downstream_port1
+
+# NVME card
+-device pcie-root-port,id=root_port_for_nvme1,chassis=2,slot=0
+ -device nvme,serial=deadbeef,bus=root_port_for_nvme1
+
+# Intel network card
+-device pcie-root-port,id=root_port_for_igb,chassis=3,slot=0
+  -device igb,bus=root_port_for_igb
+
+# XHCI USB card
+-device pcie-root-port,id=root_port_for_xhci,chassis=4,slot=0
+  -device qemu-xhci,bus=root_port_for_xhci
+
+# virtio rng
+-device pcie-root-port,id=root_port_for_rng,chassis=5,slot=0
+  -device virtio-rng-pci,bus=root_port_for_rng
+
+# PCIe-PCI bridge with es1370 behind
+-device pcie-root-port,id=root_port_for_pci,chassis=6,slot=0
+  -device pcie-pci-bridge,id=pci,bus=root_port_for_pci
+    -device es1370,bus=pci,addr=9
+
+# another PCIe bus
+ -device pxb-pcie,id=pxb1,bus_nr=1
+   -device pcie-root-port,id=root_port_for_ahci,bus=pxb1,chassis=10,slot=0
+     -device ahci,bus=root_port_for_ahci
+)
+
+
+if [ -z $NOITS ]; then
+	qemu_args="${common_qemu_args[@]} ${its_pci_setup[@]}"
+else
+	qemu_args="${common_qemu_args[@]}"
+fi
+
+if [ ! -z $GDB ]; then
+	qemu_args="${qemu_args} -s -S"
+fi
+
+if [ -z $GFX ]; then
+        qemu_args="${qemu_args} -nographic"
+fi
+
+
+echo "QEMU command line arguments:"
+echo "${qemu_args}" | sed -e "s/ -/\n-/g"
+echo ""
+
+./code/qemu/build/aarch64-softmmu/qemu-system-aarch64 $qemu_args
+
+if [ ! -z "$CMD" ]; then
         rm disks/virtual/startup.nsh
 fi
