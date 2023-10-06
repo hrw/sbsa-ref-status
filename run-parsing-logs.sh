8b3f7d390190d2509e61b4602d2072ed743fc7da
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2023-10-06 15:17:54 +0200

    do not run tests on Neoverse-N2
    It is not merged yet into QEMU.

--- run-parsing-logs.sh
+++ run-parsing-logs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for cpu in cortex-a57 cortex-a72 neoverse-n1 neoverse-v1 neoverse-n2 max
+for cpu in cortex-a57 cortex-a72 neoverse-n1 neoverse-v1 max
 do
 	python3 parse-bsa-log.py  logs/bsa-${cpu}-v1.log $cpu
 	python3 parse-bsa-log.py  logs/bsa-sbsa-${cpu}-v1.log $cpu
