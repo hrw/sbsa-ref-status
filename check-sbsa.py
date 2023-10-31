176a065e2cfe58227d262e77a133e68cd7804992
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2023-10-31 14:40:04 +0100

    check-sbsa: use xbsa checklist
    For quite a while I had two separate files with same data:
    - xbsa_checklist.yml
    - sbsa_checklist.yml
    
    They had different structure but same data. XBSA one is used also by
    generate-xbsa.py so it stays. Also structure is better.

--- check-sbsa.py
+++ check-sbsa.py
@@ -8,75 +8,84 @@ with open("status-bsa.yml", encoding="utf-8") as yml:
 with open("status-sbsa.yml", encoding="utf-8") as yml:
     status_sbsa = yaml.safe_load(yml)
 
-with open("sbsa_checklist.yml", encoding="utf-8") as yml:
-    sbsa_level_checklist = yaml.safe_load(yml)
+with open("xbsa-checklist.yml", encoding="utf-8") as yml:
+    xbsa_checklist = yaml.safe_load(yml)
+
+# some tests lists several tests and we do want to show it once
+checked_tests = {"bsa": [], "sbsa": []}
+
+def check_tag(tag, failed):
+
+    try:
+        for bsa_test_id in tags_to_tests[tag]["bsa"]:
+            if bsa_test_id not in checked_tests["bsa"]:
+                checked_tests["bsa"].append(bsa_test_id)
+
+                if status_bsa[bsa_test_id]['status'][cpu] in ["FAIL"]:
+                    print(f"- BSA  test {bsa_test_id} for "
+                            f"{status_bsa[bsa_test_id]['tags']} "
+                            f"({status_bsa[bsa_test_id]['title']}) "
+                            f"failed")
+                    failed = True
+
+        for sbsa_test_id in tags_to_tests[tag]["sbsa"]:
+            if sbsa_test_id not in checked_tests["sbsa"]:
+                checked_tests["sbsa"].append(sbsa_test_id)
+
+                if status_sbsa[sbsa_test_id]['status'][cpu] in ["FAIL"]:
+                    print(f"- SBSA test {sbsa_test_id} for "
+                        f"{status_sbsa[sbsa_test_id]['tags']} "
+                        f"({status_sbsa[sbsa_test_id]['title']}) failed")
+                    failed = True
+    except KeyError:
+        pass
+
+    return failed
+
 
 def check_sbsa_level(cpu, level, previous_level_result):
     failed = previous_level_result
 
     print(f"Checking SBSA level {level} for {cpu}")
+    checked_tests["bsa"].clear()
+    checked_tests["sbsa"].clear()
 
-    # some tests lists several tests and we do want to show it once
-    checked_tests = []
-
-    # SBSA Level 4 does not lists BSA tags
-    if sbsa_level_checklist[level]["bsa"] is not None:
-        for bsa_tag in sbsa_level_checklist[level]["bsa"]:
-            try:
-                for bsa_test_id in tags_to_tests[bsa_tag]["bsa"]:
-                    if bsa_test_id not in checked_tests:
-                        checked_tests.append(bsa_test_id)
-
-                        if status_bsa[bsa_test_id]['status'][cpu] in ["FAIL"]:
-                            print(f"- BSA  test {bsa_test_id} for "
-                                  f"{status_bsa[bsa_test_id]['tags']} "
-                                  f"({status_bsa[bsa_test_id]['title']}) "
-                                  f"failed")
-                            failed = True
-            except KeyError:
-                pass
-
-    # some tests lists several tests and we do want to show it once
-    checked_tests = []
-
-    for sbsa_tag in sbsa_level_checklist[level]["sbsa"]:
-        try:
-            for sbsa_test_id in tags_to_tests[sbsa_tag]["sbsa"]:
-                if sbsa_test_id not in checked_tests:
-                    checked_tests.append(sbsa_test_id)
-
-                    if status_sbsa[sbsa_test_id]['status'][cpu] in ["FAIL"]:
-                        print(f"- SBSA test {sbsa_test_id} for "
-                              f"{status_sbsa[sbsa_test_id]['tags']} "
-                              f"({status_sbsa[sbsa_test_id]['title']}) failed")
-                        failed = True
-        except KeyError:
-            pass
+    for category in xbsa_checklist:
+        for group in xbsa_checklist[category]["groups"]:
+            for rule in xbsa_checklist[category]["groups"][group]["rules"]:
+                if rule["required"]["sbsa"][level]:
+                    failed = check_tag(rule["tag"], failed)
 
     if not failed:
-        print(f"SBSA level {level} for {cpu}: PASS")
+        print(f"SBSA level {level} for {cpu}: no failures found")
 
     return failed
 
 
-# create a map of (S)BSA tag to tests
-tags_to_tests = {}
+def create_map_tag_to_test():
+    # create a map of (S)BSA tag to tests
+    tags_to_tests = {}
 
-for test in status_bsa:
-    for tag in status_bsa[test]["tags"].split(","):
-        tag = tag.strip()
-        if tag not in tags_to_tests:
-            tags_to_tests[tag] = {"bsa": [], "sbsa": []}
+    for test in status_bsa:
+        for tag in status_bsa[test]["tags"].split(","):
+            tag = tag.strip()
+            if tag not in tags_to_tests:
+                tags_to_tests[tag] = {"bsa": [], "sbsa": []}
 
-        tags_to_tests[tag]["bsa"].append(test)
+            tags_to_tests[tag]["bsa"].append(test)
 
-for test in status_sbsa:
-    for tag in status_sbsa[test]["tags"].split(','):
-        tag = tag.strip()
-        if tag not in tags_to_tests:
-            tags_to_tests[tag] = {"bsa": [], "sbsa": []}
+    for test in status_sbsa:
+        for tag in status_sbsa[test]["tags"].split(','):
+            tag = tag.strip()
+            if tag not in tags_to_tests:
+                tags_to_tests[tag] = {"bsa": [], "sbsa": []}
 
-        tags_to_tests[tag]["sbsa"].append(test)
+            tags_to_tests[tag]["sbsa"].append(test)
+
+    return tags_to_tests
+
+
+tags_to_tests = create_map_tag_to_test()
 
 # SBSA Level 3 requires Arm v8.0
 # SBSA Level 4 requires Arm v8.3
