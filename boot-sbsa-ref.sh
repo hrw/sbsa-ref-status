fff645758ee3dd1ee6b38f30de67c9e64cf59a73
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2023-11-13 11:35:51 +0100

    boot-sbsa-ref: show QEMU version first
    For most of time I am using upstream QEMU without extra patches
    on top. So show version as it can be used to check which exactly
    code I was using.

--- boot-sbsa-ref.sh
+++ boot-sbsa-ref.sh
@@ -325,6 +325,9 @@ fi
 
 qemu_args="${qemu_args} ${firmware_args[@]}"
 
+../code/qemu/build/aarch64-softmmu/qemu-system-aarch64 --version
+
+echo ""
 echo "QEMU command line arguments:"
 echo "${qemu_args}" | sed -e "s/ -/\n-/g"
 echo ""
