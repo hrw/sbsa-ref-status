761f2cd3c89dc5b2aacc98da16e4a554201667e7
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2024-03-14 16:34:32 +0100

    boot-sbsa-ref.py: Python version of 'run qemu' script
    Does not have complex PCIe setup yet.
    
    Uses OS images from os.yml

---
+++ boot-sbsa-ref.py
@@ -0,0 +1,212 @@
+#!/usr/bin/python3
+
+import argparse
+import os
+import random
+import string
+import sys
+import yaml
+
+from pprint import pprint
+
+
+def parse_args():
+
+    parser = argparse.ArgumentParser(description="Run virtual machine.")
+
+    parser.add_argument("--cmd", help="command to run in EFI shell")
+    parser.add_argument("--cpu", default="neoverse-v1",
+                        help="cpu core to use")
+    parser.add_argument("--gdb", action="store_true",
+                        help="wait for GDB connection")
+    parser.add_argument("--gfx", action="store_true",
+                        help="open window with graphics output")
+    parser.add_argument("--no-its", action="store_true",
+                        help="disable extra PCIe devices")
+    parser.add_argument("--no-reset", action="store_true",
+                        help="do not shutdown after running EFI command")
+    parser.add_argument("--numa", action="store_true",
+                        help="use NUMA configation")
+    parser.add_argument("--os",
+                        help="select OS to attach")
+    parser.add_argument("--secure", action="store_true",
+                        help="")
+    parser.add_argument("--virt", action="store_true",
+                        help='use "virt" instead of "sbsa-ref"')
+
+    return parser.parse_args()
+
+
+def handle_uefi_command(command, no_reset):
+    try:
+        os.remove("disks/virtual/startup.nsh")
+    except FileNotFoundError:
+        pass
+
+    with open("disks/virtual/startup.nsh", "w") as startup:
+        startup.write(f"""
+                       mode 100 31
+                       pci
+                       {command}
+                       """)
+
+        # we shutdown by default
+        if not no_reset:
+            startup.write("reset -c\n")
+
+
+def add_drive(drive_file, drive_type="", media="", drive_format="raw"):
+    drive_extra = ""
+    if drive_type:
+        drive_extra = f",if={drive_type}"
+
+    if media:
+        drive_extra += ",media=cdrom"
+
+    qemu_args.extend(["-drive",
+                     f"file={drive_file},format={drive_format}{drive_extra}"])
+
+
+def add_firmware(is_it_virt):
+    if is_it_virt:
+        add_drive("firmware/VIRT_FLASH0.fd", "pflash")
+        add_drive("firmware/VIRT_FLASH1.fd", "pflash")
+    else:
+        add_drive("firmware/SBSA_FLASH0.fd", "pflash")
+        add_drive("firmware/SBSA_FLASH1.fd", "pflash")
+
+
+def add_machine(is_it_virt):
+    if is_it_virt:
+        qemu_args.extend(["-machine", "virt,iommu=smmuv3,gic-version=max"])
+        # virt does not have USB host controller
+        add_device("qemu-xhci,id=usb")
+    else:
+        qemu_args.extend(["-machine", "sbsa-ref"])
+
+
+def add_cpu(cpu_type, is_it_numa):
+    qemu_args.extend(["-cpu", cpu_type])
+
+    if not is_it_numa:
+        pass
+        # qemu_args.extend(["-smp", "1", "-m", "1G"])
+    else:
+        qemu_args.extend([
+                        "-smp", "4,sockets=4,maxcpus=4",
+                        "-m", "4G,slots=2,maxmem=5G",
+                        "-object", "memory-backend-ram,size=1G,id=m0",
+                        "-object", "memory-backend-ram,size=3G,id=m1",
+                        "-numa", "node,nodeid=0,cpus=0-1,memdev=m0",
+                        "-numa", "node,nodeid=1,cpus=2,memdev=m1",
+                        "-numa", "node,nodeid=2,cpus=3"])
+
+
+def add_device(device_name):
+    qemu_args.extend(["-device", device_name])
+
+
+def add_usb_devices():
+    add_device("usb-kbd")
+    add_device("usb-tablet")
+    # FreeBSD prefers mouse over tablet
+    add_device("usb-mouse")
+
+
+def add_os_drive(os_name):
+    if os_name is None:
+        return
+
+    with open("os.yml") as yml:
+        os_data = yaml.safe_load(yml)
+
+    if os_name not in os_data:
+        print(f"Selected OS: {os_name} is not defined!")
+        sys.exit(-1)
+
+    if os_data[os_name]["file"].endswith(".iso"):
+        add_drive(os_data[os_name]["file"], "", "cdrom")
+    else:
+        add_drive(os_data[os_name]["file"])
+
+    if "reset" in os_data[os_name]:
+        qemu_args.append("--no-reboot")
+
+
+def enable_graphics_window(is_gfx):
+    if not is_gfx:
+        qemu_args.append("-nographic")
+
+
+def enable_gdb(is_gdb):
+    if is_gdb:
+        qemu_args.extend(["-s", "-S"])
+
+
+def show_args():
+    prev_arg = qemu_args[0]
+
+    print("QEMU command arguments:\n")
+    for arg in qemu_args:
+        if arg.startswith("-") and prev_arg.startswith("-"):
+            print(prev_arg)
+        elif arg.startswith("-"):
+            pass
+        elif prev_arg.startswith("-"):
+            print(f"{prev_arg} {arg}")
+        else:
+            print(arg)
+        prev_arg = arg
+
+    print("")
+
+
+def add_pcie(card_name, add_root_port=True):
+    global chassis
+
+    if add_root_port:
+        rpid = "".join(random.choices(string.ascii_letters, k=5))
+        qemu_args.extend(["-device",
+                         f"pcie-root-port,id={rpid},slot=0,chassis={chassis}"])
+        chassis += 1
+
+    qemu_args.extend(["-device", f"{card_name},bus={rpid}"])
+# -----------------------------------------------------------------------------
+
+args = parse_args()
+chassis = 0
+
+if args.cmd:
+    handle_uefi_command(args.cmd, args.no_reset)
+
+qemu_args = [
+    "../code/qemu/build/qemu-system-aarch64",
+    "-monitor", "telnet::45454,server,nowait",
+    "-serial", "stdio"
+]
+
+print(args)
+add_firmware(args.virt)
+add_machine(args.virt)
+add_cpu(args.cpu, args.numa)
+add_usb_devices()
+enable_graphics_window(args.gfx)
+enable_gdb(args.gdb)
+add_os_drive(args.os)
+
+# virtual drive with EFI tools
+add_drive("fat:rw:disks/virtual")
+
+# full Debian installation
+add_drive("disks/full-debian.hddimg")
+
+#add_pcie("igb")
+
+show_args()
+
+os.execv(qemu_args[0], qemu_args)
+
+try:
+    os.remove("disks/virtual/startup.nsh")
+except FileNotFoundError:
+    pass
